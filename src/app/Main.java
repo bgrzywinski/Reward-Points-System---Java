71a7a8ff91cd4aee08d100c92ea65a335d382e51
Bartek <bartek.cz3012@gmail.com>
2025-12-12 20:27:06 +0100

    fix: optimize creating objects in main

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,33 +6,30 @@ import src.basic.Tier;
 import src.business.Customer;
 import src.business.Reward;
 import src.data.Repository;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Repository repository = new Repository();
+        List<Customer> customers = repository.getCustomers();
+        List<Reward> rewards = repository.getRewards();
 
         Address address = new Address(1L, "Krakow", "Rynek", "10-001");
-        Customer customer = new Customer(1L, "Jan Kowalski", address, Tier.Gold);
-
         Address address2 = new Address(2L, "Radom", "Rynek", "20-001");
-        Customer customer2 = new Customer(2L, "Kamil Ślimak", address2, Tier.Bronze, 300.0);
-
         Address address3 = new Address(3L, "Sosnowiec", "Rynek", "30-001");
-        Customer customer3 = new Customer(3L, "Robert Lewandowski", address3, Tier.Gold, 150.0);
+        Address address4 = new Address(4L, "Częstochowa", "Stadion Rakowa", "123");
 
-        Address address4 = new Address(3L, "Częstochowa", "Stadion Rakowa", "123");
-        Customer customer4 = new Customer(3L, "Marcin Najman", address4, Tier.Silver, 200.0);
+        customers.add(new Customer(1L, "Jan Kowalski", address, Tier.Gold));
+        customers.add(new Customer(2L, "Kamil Ślimak", address2, Tier.Bronze, 300.0));
+        customers.add(new Customer(3L, "Robert Lewandowski", address3, Tier.Gold, 150.0));
+        customers.add(new Customer(4L, "Marcin Najman", address4, Tier.Silver, 200.0));
 
-        repository.getCustomers().add(customer);
-        repository.getCustomers().add(customer2);
-        repository.getCustomers().add(customer3);
-        repository.getCustomers().add(customer4);
-
-        repository.getRewards().add(new Reward(1L, "Pluszowy Miś", 1, 100.0));
-        repository.getRewards().add(new Reward(2L, "Kupon 50PLN", 1, 500.0));
-        repository.getRewards().add(new Reward(3L, "Wycieczka do Lichenia", 1, 5000.0));
-        repository.getRewards().add(new Reward(4L, "Lamborghini", 1, 1000.0));
+        rewards.add(new Reward(1L, "Pluszowy Miś", 1, 100.0));
+        rewards.add(new Reward(2L, "Kupon 50PLN", 1, 500.0));
+        rewards.add(new Reward(3L, "Wycieczka do Lichenia", 1, 5000.0));
+        rewards.add(new Reward(4L, "Lamborghini", 1, 1000.0));
 
         boolean running = true;
 
